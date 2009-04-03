66e3e49c252160d35886bab0bfee226bcb7b7b15
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-04-03 19:56:36 +0000

    correcting last commit (note: i'm still very tired)
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@14 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/messages/viewlist.php
+++ system/application/views/messages/viewlist.php
@@ -1,16 +1,21 @@
 <?php
-if (!empty($messages)) {
+if (!empty($messages)) 
+{
 	
-	foreach ($messages as $message) {
-		if (!empty($message)) {
+	foreach ($messages as $message) 
+	{
+		if (!empty($message)) 
+		{
 			$aux = explode("|", $message);
 		    $username = $aux[0];
-			if (stristr($_SERVER['REQUEST_URI'],'users/home') && $username != $User['username'])
-				next;
-		    $time = $aux[1];
-		    $message = $aux[2];
-		  	//echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"/users/view/$username\">$username</a> $message <span>$time</span></div>";
-			echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"#\">$username</a> $message <span>" . $this->time->timeAgo($time) . " ago</span></div>";
+			
+			if (!(stristr($_SERVER['REQUEST_URI'],'users/home') && $username != $User['username']))
+			{
+			    $time = $aux[1];
+			    $message = $aux[2];
+			  	//echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"/users/view/$username\">$username</a> $message <span>$time</span></div>";
+				echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"#\">$username</a> $message <span>" . $this->time->timeAgo($time) . " ago</span></div>";
+			}
 		}
 	}
 }
