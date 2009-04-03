56fa3bd521b9e69a8315910811bc56369d19d4d8
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-04-03 19:47:09 +0000

    added dirty personal timelines to `users/home`
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@12 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/messages/viewlist.php
+++ system/application/views/messages/viewlist.php
@@ -1,9 +1,12 @@
 <?php
 if (!empty($messages)) {
+	
 	foreach ($messages as $message) {
 		if (!empty($message)) {
 			$aux = explode("|", $message);
 		    $username = $aux[0];
+			if (stristr($_SERVER['REQUEST_URI'],'users/home') && $username != $User['username'])
+				break;
 		    $time = $aux[1];
 		    $message = $aux[2];
 		  	//echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"/users/view/$username\">$username</a> $message <span>$time</span></div>";
--- system/application/views/users/home.php
+++ system/application/views/users/home.php
@@ -1,7 +1,7 @@
-<h2>What are you doing?</h2>
+<h2>Hello <?php echo $User['username']  ?></h2>
+<h3>What are you doing?</h3>
 <form action="/messages/add" method="post" accept-charset="utf-8">
 	<textarea name="message" rows="8" cols="40"></textarea>
 	<p><input type="submit" value="Update"></p>
 </form>
-<h3>Hello <?php echo $User['username']  ?></h3>
 <?php echo $this->load->view('messages/viewlist') ?>
