773f86865a0c75d3015190e4d72bd7dc2c9b0be4
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-04-03 19:51:32 +0000

    updated `break` to `next` because we don't want to just stop looping if there's another post (this can be done much more elegantly by prefiltering `$messages`)
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@13 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/messages/viewlist.php
+++ system/application/views/messages/viewlist.php
@@ -6,7 +6,7 @@ if (!empty($messages)) {
 			$aux = explode("|", $message);
 		    $username = $aux[0];
 			if (stristr($_SERVER['REQUEST_URI'],'users/home') && $username != $User['username'])
-				break;
+				next;
 		    $time = $aux[1];
 		    $message = $aux[2];
 		  	//echo $userlink = "<div class=\"message\"><a class=\"username\" href=\"/users/view/$username\">$username</a> $message <span>$time</span></div>";
