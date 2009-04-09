5811f8e10e81e8f781704a632833aad229586a41
johnmenick <johnmenick@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-04-09 15:34:42 +0000

    Fixed random string problem
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@27 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/helpers/Selenium.php
+++ system/application/helpers/Selenium.php
@@ -147,9 +147,9 @@ class Selenium
 	*/
 	public function randomString($length) {
 		$randstr = null;
-		srand((double)microtime()*1000000);
+		srand();
 		$chars = array( 'a','b','c','d','e','f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A','B','C','D','E','F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9');
-		for ($rand = 0; $rand <= $length; $rand++) {
+		for ($rand = 0; $rand < $length; $rand++) {
 			$random = rand(0, count($chars) -1);
 			$randstr .= $chars[$random];
 		}
