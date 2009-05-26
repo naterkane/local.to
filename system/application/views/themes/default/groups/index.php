0ae1373ab3ffea07fe72a3dadd0cda5d44a78d97
johnmenick <johnmenick@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-05-26 21:25:31 +0000

    Fixed group link on index
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@150 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/default/groups/index.php
+++ system/application/views/themes/default/groups/index.php
@@ -4,7 +4,7 @@
 		<a href="/groups/add" class="toggler">Create a group</a>
 	<?php if (!empty($groups)): ?>
 		<?php foreach ($groups as $group): ?>
-			<p><a href="/group/<?php echo $group ?>"><?php echo $group['name'] ?></a></p>		
+			<p><a href="/group/<?php echo $group['name'] ?>"><?php echo $group['name'] ?></a></p>		
 		<?php endforeach ?>
 	<?php else: ?>
 			<p>There are no groups yet.</p>
