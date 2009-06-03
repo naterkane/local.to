8f74f9bee5537bba2fb9a4983dae28e1d84a8ae7
johnmenick <johnmenick@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-03 00:40:24 +0000

    Fixed group views [#125 state:resolved]
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@184 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/default/groups/view.php
+++ system/application/views/themes/default/groups/view.php
@@ -1,5 +1,5 @@
 <div class="box messages">
-	<h2><?php echo $name ?> </h2>
+	<h2><?php echo $group['name'] ?> </h2>
 	<div class="box">
 		<div class="block">
 			<?php
@@ -8,37 +8,37 @@
 			 */
 			// start move to sidebar
 			?>
-			<p><a href="/groups/members/<?php echo $name ?>">Members (<?php echo $member_count ?>)</a></p>
+			<p><a href="/groups/members/<?php echo $group['name'] ?>">Members (<?php echo $group['member_count'] ?>)</a></p>
 			<?php if (!empty($url)){
 				 echo "<p><a href=\""; 
 				 echo (substr($url,0,7) == "http://")?$url:"http://".$url;
 				 echo"\">$url</a></p>"; 
 			}?>
-			<?php if (!empty($desc)) echo "<p>$desc</p>"; ?>
+			<?php if (!empty($group['desc'])) echo "<p>" . $group['desc'] . "</p>"; ?>
 			<?php if (!empty($location)) echo "<p><strong>Location:</strong> $location</p>"; ?>
 			<p>
-				<?php if (!$is_owner && $this->userData): ?>
-					<?php if ($im_a_member): ?>
-							<a href="/groups/unsubscribe/<?php echo $id ?>" id="unsubscribe" class="toggler">Unsubscribe</a>
+				<?php if (!$group['is_owner'] && $this->userData): ?>
+					<?php if ($group['im_a_member']): ?>
+							<a href="/groups/unsubscribe/<?php echo $group['id'] ?>" id="unsubscribe" class="toggler">Unsubscribe</a>
 					<?php else: ?>
-							<a href="/groups/subscribe/<?php echo $id ?>" id="subscribe" class="toggler">Subscribe</a>
+							<a href="/groups/subscribe/<?php echo $group['id'] ?>" id="subscribe" class="toggler">Subscribe</a>
 					<?php endif ?>
 				<?php endif ?>
 				
-				<?php if ($is_owner): ?>
-				<a href="/groups/settings/<?php echo $name ?>" class="toggler">Edit <?php echo (substr($name,-1) == "s")?$name."'":$name."'s"; ?> Profile</a>
+				<?php if ($group['is_owner']): ?>
+				<a href="/groups/settings/<?php echo $group['name'] ?>" class="toggler">Edit <?php echo (substr($group['name'],-1) == "s")?$group['name']."'":$group['name']."'s"; ?> Profile</a>
 				<?php endif ?>
 			</p>
 			<?php
 			// end move to sidebar
 			
-			if ($im_a_member): ?>
+			if (isset($group['im_a_member'])): ?>
 			<form action="/messages/add" method="post" accept-charset="utf-8">
-			<h3>Post a message to <?php echo $name ?></h3>
+			<h3>Post a message to <?php echo $group['name'] ?></h3>
 				<fieldset>
 				<?php 
 				$messagedata = array();
-				$messagedata['message'] = "!".$name;
+				$messagedata['message'] = "!".$group['name'];
 				echo $this->load->view('messages/postform',$messagedata) ?>
 				</fieldset>
 			</form>
