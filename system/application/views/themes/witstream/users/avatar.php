4f58af7ae7617a56b2e9f485b1af26cc69e225a0
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-09 22:01:49 +0000

    bandaided avatar form issues
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@211 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/witstream/users/avatar.php
+++ system/application/views/themes/witstream/users/avatar.php
@@ -1,10 +1,9 @@
-<div class="box messages">
+<div class="settings">
 	<div class="top short">
-		<h3>Upload your avatar</h2>
+		<h3>Upload your Photo!</h2>
 	</div>
-	<div class="box"><div class="box"><div class="box">
-		
-		<?php echo $avatar->user($user, "50" ); ?>
+	<div class="box content">
+	<div class="avatar-original"><?php echo $avatar->user($this->userData, "original" ); ?></div>
 	<form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER['REQUEST_URI'] ?>">
 		<fieldset>
 	<p>
@@ -15,5 +14,5 @@
 	</p>
 	</fieldset>
 	</form>
-	</div></div></div>
+	</div>
 </div>
