d1dc9e024c1c7f75e19c9fc43e7b6c88eec96c8d
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-09 04:05:13 +0000

    organized the routes file, and added support for varying nomenclature for inbox/outbox taxonomy
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@206 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/config/routes.php
+++ system/application/config/routes.php
@@ -44,37 +44,57 @@ $route['default_controller'] = "welcome";
 $route['scaffolding_trigger'] = "";
 
 // tests
-$route['tests/:any'] = "tests/index";
-$route['testme'] = "tests/testme";
+$route['tests/:any'] = 	"tests/index";
+$route['testme'] = 		"tests/testme";
 
-// static pages
-$route['about'] = "welcome/page/about";
-$route['home'] = "users/home";
-$route['home/(:any)'] = "users/home/$1";
-$route['group/(:any)'] = "groups/view/$1";
-$route['signout'] = "users/signout";
-$route['signin'] = "users/signin";
-$route['signup/(:any)/(:any)'] = "users/signup/$1/$2";
-$route['delete'] = "users/delete";
-$route['confirm/(:any)'] = "users/confirm/$1";
-$route['deny/(:any)'] = "users/deny/$1";
-$route['friend_requests'] = "users/friend_requests";
-$route['settings'] = "users/settings";
-$route['settings/sms'] = "users/sms";
-$route['public_timeline'] = "messages/public_timeline";
+/**
+ * Static pages
+ * @todo need to make this dynamic/flexible in the future
+ */
+$route['about'] = 	"welcome/page/about";
+$route['legal'] = 	"welcome/page/legal";
+$route['terms'] = 	"welcome/page/terms";
+$route['contact'] = "welcome/page/contact";
+$route['faq'] = 	"welcome/page/faq";
+
+
+$route['request_invite'] = 			"admin/request_invite";
+$route['home'] = 					"users/home";
+$route['home/(:any)'] = 			"users/home/$1";
+$route['rss/user/(:any)'] = 		"users/rss/$1";
+$route['signout'] = 				"users/signout";
+$route['signin'] = 					"users/signin";
+$route['signup/(:any)/(:any)'] = 	"users/signup/$1/$2";
+$route['delete'] = 					"users/delete";
+$route['confirm/(:any)'] = 			"users/confirm/$1";
+$route['deny/(:any)'] = 			"users/deny/$1";
+$route['friend_requests'] = 		"users/friend_requests";
+$route['mentions'] = 				"users/mentions";
+$route['favorites'] = 				"users/favorites";
+$route['settings'] = 				"users/settings";
+$route['settings/sms'] = 			"users/sms";
+$route['settings/avatar'] = 		"users/avatar";
+$route['avatar'] = 					"users/avatar";
+$route['change_password'] = 		"users/change_password";
+$route['recover_password'] = 		"users/recover_password";
+$route['reset_password/(:any)'] = 	"users/reset_password/$1";
+$route['reset/(:any)'] = 			"users/reset/$1";
+
+$route['inbox'] = 					"messages/inbox";
+$route['received'] = 				"messages/inbox";
+$route['outbox'] = 					"messages/sent";
+$route['sent'] = 					"messages/sent";
+$route['public_timeline'] = 		"messages/public_timeline";
+
+$route['group/(:any)'] = 			"groups/view/$1";
+$route['img/(:any)'] = 				"assets/img/$1";
+
+/**
+ * @depreciated do not use
+ */
 $route['public_timeline_threaded'] = "messages/public_timeline_threaded";
-$route['reset/(:any)'] = "users/reset/$1";
-$route['change_password'] = "users/change_password";
-$route['request_invite'] = "admin/request_invite";
-$route['inbox'] = "messages/inbox";
-$route['sent'] = "messages/sent";
-$route['rss/user/(:any)'] = "users/rss/$1";
-$route['img/(:any)'] = "assets/img/$1";
-$route['avatar'] = "users/avatar";
-$route['recover_password'] = "users/recover_password";
-$route['reset_password/(:any)'] = "users/reset_password/$1";
-$route['mentions'] = "users/mentions";
-$route['favorites'] = "users/favorites";
+
+
 
 /* End of file routes.php */
 /* Location: ./system/application/config/routes.php */
