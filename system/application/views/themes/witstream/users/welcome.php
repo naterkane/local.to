ac96c624d2cd40b1a89dcf245602a9319af98e80
johnmenick <johnmenick@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-10 15:58:14 +0000

    Fixing welcome messages
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@222 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/witstream/users/welcome.php
+++ system/application/views/themes/witstream/users/welcome.php
@@ -2,4 +2,7 @@
 	<h2>Sign In</h2>
 	<?php echo $this->load->view('users/signin_form') ?>
 </div>
-<?php echo $this->load->view('admin/request_invite') ?>
+<?php echo $this->load->view('messages/viewlist') ?>
+<div class="threading">
+<?php $this->load->view('users/toggle_threading',array('threading'=>($this->userData)?$this->userData['threading']:0)); ?>
+</div>
