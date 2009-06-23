31b5443a9a429c212c4f165239a576d96ffdfc4f
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-23 20:16:08 +0000

    added static pages, and updated footer nav
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@243 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/default/layouts/footer.php
+++ system/application/views/themes/default/layouts/footer.php
@@ -1,6 +1,7 @@
 <div class="clear"></div>
 <div class="grid_16" id="site_info">
 	<div class="box">
+		<p><a href="/about">about</a>  ·  <a href="/legal">legal</a>  ·  <a href="/terms">terms &amp; conditions</a>  ·  <a href="/faq">faq</a>  ·  <a href="/contact">contact</a></p>
 		<p><a href="http://wearenom.com/software/nomcat">NomCat</a>, a micropublishing platform, created by <a href="http://wearenom.com">NOM</a>. 
 		<!--<br />This <span xmlns:dc="http://purl.org/dc/elements/1.1/" href="http://purl.org/dc/dcmitype/InteractiveResource" rel="dc:type">work</span> is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/us/">Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 United States License</a>.
 		--><a rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/us/"><img alt="Creative Commons License" style="border-width:0;float:right;" src="http://i.creativecommons.org/l/by-nc-nd/3.0/us/80x15.png" /></a>
---
+++ system/application/views/themes/default/static/contact.php
@@ -0,0 +1,24 @@
+<div class="box">
+    <div class="block">
+        <h1>Contact</h1>
+        <div id="" class="vcard">
+            <a class="url fn n" href="http://wearenom.com"><span class="given-name"></span><span class="additional-name"></span><span class="family-name"></span></a>
+            <div class="org">
+                NOM
+            </div>
+            <a class="email" href="mailto:info@wearenom.com">info@wearenom.com</a>
+            <div class="adr">
+                <div class="street-address">
+                    964 Flushing Ave
+                </div>
+                <span class="locality">Brooklyn</span>
+                , <span class="region">NY</span>
+                , <span class="postal-code">11206</span>
+                <span class="country-name">United States</span>
+            </div>
+            <div class="tel">
+                +1 (917) 267-7825
+            </div>
+        </div>
+    </div>
+</div>
