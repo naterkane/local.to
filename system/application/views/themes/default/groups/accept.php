6094c39f8242b881050b71e77a3b801a92e52538
johnmenick <johnmenick@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-06-29 19:27:39 +0000

    Added a link to sign in for groups [#149 state: resolved]
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@259 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- system/application/views/themes/default/groups/accept.php
+++ system/application/views/themes/default/groups/accept.php
@@ -1,4 +1,4 @@
-<p>You must sign up or sign in to join a group.</p>
+<p>You must sign up or sign in to join a group. Already a member? <?php echo $html->link('sign in', '/signin?redirect=/groups/accept/' . $key) ?></p>
 <div class="box">
 	<h2>Sign Up</h2>
 	<div class="block">
