1231ba14dbc0a720616250ba2f14db47f8adc34e
naterkane <naterkane@874695d1-85b0-4378-8154-42b2b3dd7dfc>
2009-11-03 04:34:10 +0000

    updated console.log wrapper to match Paul Irish's example from http://paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
    git-svn-id: https://nom.svn.beanstalkapp.com/microblog/trunk@373 874695d1-85b0-4378-8154-42b2b3dd7dfc

--- webroot/js/nomcat.js
+++ webroot/js/nomcat.js
@@ -1,8 +1,7 @@
 /**
  * @author naterkane
  */
-if(!(('console' in window) && ('log' in console)))
-    window.console = { log: function(){} };
+window.log=function(){var a="history";log[a]=log[a]||[];log[a].push(arguments);window.console&&console.log[console.firebug?"apply":"call"](console,Array.prototype.slice.call(arguments))};window.logargs=function(a){log(a,arguments.callee.caller.arguments)};
 $(document).ready(function(){
 		// hides a flashmessage shortly after loading
 		$("#flashMessage").slideDown(1000);
